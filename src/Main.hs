73dbb71611a871379e597a1355012a3dfcb7f766
Geoff Hamilton <hamilton@computing.dcu.ie>
2021-03-27 12:33:50 +0000

    Update Main.hs
    Some bug fixes.

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,11 @@ module Main (
     main
 ) where
 
-import Termcheck
+import Exception
 import Term
-import Transform
+import Trans
+import Termcheck
+
 import Text.ParserCombinators.Parsec
 import Debug.Trace
 import System.Directory
@@ -12,11 +14,13 @@ import System.IO
 import Control.Monad
 import Data.List
 import System.Exit
+import System.Process
 
 data Command = Load String
              | Prog
              | Term
              | Eval
+             | Distill
              | Termcheck
              | Quit
              | Help
@@ -28,6 +32,7 @@ command str = let res = words str
                    [":prog"] -> Prog
                    [":term"] -> Term
                    [":eval"] -> Eval
+                   [":distill"] -> Distill
                    [":termcheck"] -> Termcheck
                    [":quit"] -> Quit
                    [":help"] -> Help
@@ -36,8 +41,9 @@ command str = let res = words str
 helpMessage = "\n:load filename\t\tTo load the given filename\n"++
                ":prog\t\t\tTo print the current program\n"++
                ":term\t\t\tTo print the current term\n"++
-               ":eval\t\t\tTo evaluate the current term\n"++
-               ":termcheck\t\tTo termination check the current program\n"++
+               ":eval\t\t\tTo evaluate the current program\n"++
+               ":distill\t\tTo distill the current program\n"++
+               ":termcheck\t\tTo check the termination of the current program\n"++
                ":quit\t\t\tTo quit\n"++
                ":help\t\t\tTo print this message\n"
 
@@ -53,49 +59,51 @@ toplevel p = do putStr "POT> "
                 case command x of
                    Load f -> g [f] [] []
                              where
-                             g [] ys ds = let ds' = makeFuns ds
-                                          in  case lookup "main" ds' of
-                                                 Nothing -> do putStrLn "No main function"
-                                                               toplevel Nothing
-                                                 Just (xs,t) -> toplevel (Just (t,ds'))
-                             g (x:xs) ys ds = if   x `elem` ys
-                                              then g xs ys ds
-                                              else do r <- loadFile x
-                                                      case r of
-                                                         Nothing -> toplevel Nothing
-                                                         Just (fs,ds2) -> g (xs++fs) (x:ys) (ds++ds2)
+                             g [] ys d = toplevel (Just (makeProg d))
+                             g (x:xs) ys d = if   x `elem` ys
+                                             then g xs ys d
+                                             else do r <- loadFile x
+                                                     case r of
+                                                        Nothing -> toplevel Nothing
+                                                        Just (fs,d') -> g (xs++fs) (x:ys) (d++d')
                    Prog -> case p of
                               Nothing -> do putStrLn "No program loaded"
                                             toplevel p
-                              Just (t,ds) -> do putStrLn (showProg (t,ds))
-                                                toplevel p
+                              Just (t,d) -> do putStrLn (showProg (t,d))
+                                               toplevel p
                    Term -> case p of
                               Nothing -> do putStrLn "No program loaded"
                                             toplevel p
-                              Just (t,ds) -> do print t
-                                                toplevel p
+                              Just (t,d) -> do print t 
+                                               toplevel p
                    Eval -> case p of
                               Nothing -> do putStrLn "No program loaded"
                                             toplevel p
-                              Just (t,ds) -> f (free t) t
-                                             where
-                                             f [] t = do let (v,r,a) = eval t EmptyCtx ds 0 0
-                                                         print v
-                                                         putStrLn ("Reductions: " ++ show r)
-                                                         putStrLn ("Allocations: " ++ show a)
-                                                         toplevel p
-                                             f (x:xs) t = do putStr (x++" = ")
-                                                             hFlush stdout
-                                                             l <-  getLine
-                                                             case parseTerm l of
-                                                                Left s -> do putStrLn ("Could not parse term: "++ show s)
-                                                                             f (x:xs) t
-                                                                Right u -> f xs (subst u (abstract t x))
+                              Just (t,d) -> f (free t) t
+                                            where
+                                            f [] t = do let (v,r,a) = eval t EmptyCtx d 0 0
+                                                        print v
+                                                        putStrLn ("Reductions: " ++ show r)
+                                                        putStrLn ("Allocations: " ++ show a)
+                                                        toplevel p
+                                            f (x:xs) t = do putStr (x++" = ")
+                                                            hFlush stdout
+                                                            l <-  getLine
+                                                            case parseTerm l of
+                                                               Left s -> do putStrLn ("Could not parse term: "++ show s)
+                                                                            f (x:xs) t
+                                                               Right u -> f xs (subst u (abstract t x))
+                   Distill -> case p of
+                                 Nothing -> do putStrLn "No program loaded"
+                                               toplevel p
+                                 Just (t,d) -> do let p' = dist (t,d)
+                                                  putStrLn (showProg p')
+                                                  toplevel (Just p')
                    Termcheck -> case p of
-                               Nothing -> do putStrLn "No program loaded"
-                                             toplevel p
-                               Just t -> do print (check (dist t))
-                                            toplevel p
+                                   Nothing -> do putStrLn "No program loaded"
+                                                 toplevel p
+                                   Just (t,d) -> do print (check (t,d))
+                                                    toplevel p
                    Quit -> return ()
                    Help -> do putStrLn helpMessage
                               toplevel p
@@ -105,12 +113,3 @@ toplevel p = do putStr "POT> "
 loadFile :: String -> IO (Maybe ([String],[(String,([String],Term))]))
 
 loadFile f = do x <-  doesFileExist (f++".pot")
-                if   x
-                     then do putStrLn ("Loading file: "++f++".pot")
-                             c <-  readFile (f++".pot")
-                             case parseModule c of
-                                Left s -> do putStrLn ("Could not parse program in file "++f++".pot: "++ show s)
-                                             return Nothing
-                                Right t -> return (Just t)
-                     else do putStrLn ("No such file: "++f++".pot")
-                             return Nothing
